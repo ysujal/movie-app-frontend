51ee39f14bed3bbc536ad885d0d327c368a1bdc6
Sujal Yadav <your-email@example.com>
2025-05-18 14:40:08 +0530

    Changed file extensions from js to jsx and other updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 import "./App.css";
 import GenreMovieList from "./components/GenreMovieList";
 import Header from "./components/Header";
@@ -5,15 +7,35 @@ import ProductionHouse from "./components/ProductionHouse";
 import Slider from "./components/Slider";
 import Footer from "./components/Footer";
 
+import Login from "./Pages/Login"; 
+import Signup from "./Pages/Signup"; 
+
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <Slider />
-      <ProductionHouse />
-      <GenreMovieList />
+
+      {/* Navigation links to switch pages */}
+      <nav style={{ padding: "1rem", background: "#eee" }}>
+        <Link to="/" style={{ marginRight: "1rem" }}>Home</Link>
+        <Link to="/login" style={{ marginRight: "1rem" }}>Login</Link>
+        <Link to="/signup">Signup</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Slider />
+            <ProductionHouse />
+            <GenreMovieList />
+          </>
+        } />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+
       <Footer />
-    </>
+    </Router>
   );
 }
 
